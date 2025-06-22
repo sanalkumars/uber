0cf993bca69feb07138c1b8723fffa2dca708440
sanalkumars <sanalskumar126@gmail.com>
2025-06-22 14:23:56 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+const cors = require("cors")
+
+dotenv.config();
+const express = require("express")
+
+
+const app = express();
+
+app.use(cors());  // configuring cors
+
+
+// tesing sever response 
+
+app.get('/',(req, res)=>{
+    res.send("Hello world");
+})
+
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const http = require("http")
+const app = require("./app")
+
+
+
+const server = http.createServer(app)
+
+const port = process.env.PORT||3001;
+
+
+server.listen(port,()=>{
+    console.log(`Server running at ${port}`);
+})
